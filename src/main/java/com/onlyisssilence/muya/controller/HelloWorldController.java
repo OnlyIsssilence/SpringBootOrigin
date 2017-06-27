2236673b2e73c772ffb8befb1b3009a68a892b0b
onlyisssilence <981411866@qq.com>
2017-06-27 23:47:59 +0800

    1.工程结构创建： 1.1.Application.java 建议放到跟目录下面,主要用于做一些框架配置 1.2.domain目录主要用于实体（Entity）与数据访问层（Repository） 1.3.service 层主要是业务类代码 1.4.controller 负责页面访问控制 2.引入web模块

---
+++ src/main/java/com/onlyisssilence/muya/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.onlyisssilence.muya.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: MuYa
+ * Date: 2017-06-27
+ * Time: 下午 11:43
+ */
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
