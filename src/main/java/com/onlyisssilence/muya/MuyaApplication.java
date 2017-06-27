2db0dfebcd2d0822c75fd434d22d9cd656afd0fd
onlyisssilence <981411866@qq.com>
2017-06-27 23:27:33 +0800

    spring boot初始化框架代码

---
+++ src/main/java/com/onlyisssilence/muya/MuyaApplication.java
@@ -0,0 +1,12 @@
+package com.onlyisssilence.muya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyaApplication.class, args);
+	}
+}
